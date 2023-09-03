82aef8cb2d2df1b10f2b684eae5892d372fc4e41
Maxkambek <mahkamkasimov94@gmail.com>
2023-09-03 22:29:15 +0500

    property

--- a/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Blog, BlogCategory, PortfolioCategory, Portfolio, Quiz
-
-admin.site.register(BlogCategory)
-admin.site.register(Blog)
-admin.site.register(PortfolioCategory)
-admin.site.register(Portfolio)
-admin.site.register(Quiz)
